9ab543af3ab78586b23f32de83883673158c4417
Umair Khan <umair.k2607@gmail.com>
2023-01-07 12:55:06 +0500

    client side proof

--- client/index.js
+++ client/index.js
@@ -1,17 +1,24 @@
-const axios = require('axios');
-const niceList = require('../utils/niceList.json');
-const MerkleTree = require('../utils/MerkleTree');
+const axios = require("axios");
+const niceList = require("../utils/niceList.json");
+const MerkleTree = require("../utils/MerkleTree");
 
-const serverUrl = 'http://localhost:1225';
+const serverUrl = "http://localhost:1225";
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
-
-  const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
-  });
-
-  console.log({ gift });
+  const merkleTree = new MerkleTree(niceList);
+  const root = merkleTree.getRoot();
+  const name = "Norman Block";
+  const index = niceList.findIndex((n) => n === name);
+  if (index) {
+    const proof = merkleTree.getProof(index);
+    const { data: gift } = await axios.post(`${serverUrl}/gift`, {
+      proof,
+      name,
+      root,
+    });
+    console.log({ gift });
+  } else {
+    console.log("Sorry you are not in the list");
+  }
 }
-
 main();
